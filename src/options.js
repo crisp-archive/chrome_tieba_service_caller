146f6b9787abdda04ae319ad537e281695e27ecc
crisp <crispgm@gmail.com>
2014-07-09 18:21:27 +0800

    set no expand tab

--- src/options.js
+++ src/options.js
@@ -8,223 +8,223 @@
  **/
 
 defaultConfig = {
-	"config": {
-		"service" :[
-			{ "service": "forum", "method": "getBtxInfoByName", "param": "forum_name" },
-			{ "service": "forum", "method": "getBtxInfo", "param": "forum_id" },
-			{ "type": "separator" },
-			{ "service": "user", "method": "getUserData", "param": "user_id" },
-			{ "service": "user", "method": "getUserDataExByUname", "param": "user_name" },
-			{ "service": "user", "method": "getMyFavorForum", "param": "user_id" },
-			{ "type": "separator" },
-			{ "service": "perm", "method": "getForumMemberInfo", "param": "forum_id" },
-			{ "service": "perm", "method": "getBawuList", "param": "forum_id" },
-			{ "service": "perm", "method": "getUserBawuForum", "param": "user_id" }
-		],
-		"domain": "http://service.tieba.baidu.com",
-		"format": "json"
-	}
+    "config": {
+        "service" :[
+            { "service": "forum", "method": "getBtxInfoByName", "param": "forum_name" },
+            { "service": "forum", "method": "getBtxInfo", "param": "forum_id" },
+            { "type": "separator" },
+            { "service": "user", "method": "getUserData", "param": "user_id" },
+            { "service": "user", "method": "getUserDataExByUname", "param": "user_name" },
+            { "service": "user", "method": "getMyFavorForum", "param": "user_id" },
+            { "type": "separator" },
+            { "service": "perm", "method": "getForumMemberInfo", "param": "forum_id" },
+            { "service": "perm", "method": "getBawuList", "param": "forum_id" },
+            { "service": "perm", "method": "getUserBawuForum", "param": "user_id" }
+        ],
+        "domain": "http://service.tieba.baidu.com",
+        "format": "json"
+    }
 };
 
 function getConfig()
 {
-	chrome.storage.local.get("config", function(data){
-		service = data.config["service"];
-		domain  = data.config["domain"];
-		format  = data.config["format"];
-		showConfig();
-	});
+    chrome.storage.local.get("config", function(data){
+        service = data.config["service"];
+        domain  = data.config["domain"];
+        format  = data.config["format"];
+        showConfig();
+    });
 }
 
 function showConfig()
 {
-	document.querySelector("#domain").value = domain;
-	document.querySelector("#format").value = format;
-	var config_html = "<tr><td>Service</td><td>Method</td><td>Parameter</td><td>Operation</td></tr>";
-	for(var i in service){
-		var service_name = service[i]["service"];
-		var method = service[i]["method"];
-		var param = service[i]["param"];
-		if (typeof service[i]["type"] != "undefined"){
-			if ( service[i]["type"] == "separator"){
-				config_html += "<tr><td>----</td><td></td><td></td><td>"+generateButton(i)+"</tr>";
-				continue;
-			}
-		}
-		config_html += "<tr><td>"+service_name+"</td><td>"+method+"</td><td>"+param+"<td>"+generateButton(i)+"</td></tr>";
-	}
-	document.querySelector("#get").innerHTML = config_html;
-	for(var i in service){
-		addButtonListener(i);
-	}
-	generateContextMenu();
+    document.querySelector("#domain").value = domain;
+    document.querySelector("#format").value = format;
+    var config_html = "<tr><td>Service</td><td>Method</td><td>Parameter</td><td>Operation</td></tr>";
+    for(var i in service){
+        var service_name = service[i]["service"];
+        var method = service[i]["method"];
+        var param = service[i]["param"];
+        if (typeof service[i]["type"] != "undefined"){
+            if ( service[i]["type"] == "separator"){
+                config_html += "<tr><td>----</td><td></td><td></td><td>"+generateButton(i)+"</tr>";
+                continue;
+            }
+        }
+        config_html += "<tr><td>"+service_name+"</td><td>"+method+"</td><td>"+param+"<td>"+generateButton(i)+"</td></tr>";
+    }
+    document.querySelector("#get").innerHTML = config_html;
+    for(var i in service){
+        addButtonListener(i);
+    }
+    generateContextMenu();
 }
 
 function generateButton(i)
 {
-	return "<button id=\"up_"+i+"\">Up</button><button id=\"down_"+i+"\">Down</button><button id=\"del_"+i+"\">Del</button>";
+    return "<button id=\"up_"+i+"\">Up</button><button id=\"down_"+i+"\">Down</button><button id=\"del_"+i+"\">Del</button>";
 }
 
 function addButtonListener(i)
 {
-	document.querySelector("#up_"+i).addEventListener("click", opClickHandler);
-	document.querySelector("#down_"+i).addEventListener("click", opClickHandler);
-	document.querySelector("#del_"+i).addEventListener("click", opClickHandler);
+    document.querySelector("#up_"+i).addEventListener("click", opClickHandler);
+    document.querySelector("#down_"+i).addEventListener("click", opClickHandler);
+    document.querySelector("#del_"+i).addEventListener("click", opClickHandler);
 }
 
 function generateContextMenu()
 {
-	var contexts = ["selection"];
-	chrome.contextMenus.removeAll();
-	// create parent
-	chrome.contextMenus.create({
-		"title": "Tieba_Service::call",
-		"id": "parent",
-		"contexts": contexts
-	});
-	// go to
-	chrome.contextMenus.create({
-		"title": "goto",
-		"parentId": "parent",
-		"id": "goto",
-		"contexts": contexts
-	});
-	// timestamp to time
-	chrome.contextMenus.create({
-		"title": "timestamp2time",
-		"parentId": "parent",
-		"id": "timestamp",
-		"contexts": contexts
-	});
-	// long to ip
-	chrome.contextMenus.create({
-		"title": "long2ip",
-		"parentId": "parent",
-		"id": "long2ip",
-		"contexts": contexts
-	});
-	// ip to long
-	chrome.contextMenus.create({
-		"title": "ip2long",
-		"parentId": "parent",
-		"id": "ip2long",
-		"contexts": contexts
-	});
-	chrome.contextMenus.create({
-		"type": "separator",
-		"parentId": "parent",
-		"id": "separator_sys",
-		"contexts": contexts
-	});
-	for(var i in service){
-		if ( typeof service[i]["type"] != "undefined" ){
-			if (service[i]["type"] == "separator")
-			{
-				chrome.contextMenus.create({
-					"type": "separator",
-					"id": "separator"+i,
-					"parentId": "parent",
-					"contexts": contexts
-				},
-				function(){
-					alert(chrome.runtime.lastError.message);
-				});
-				continue;
-			}
-		}
-		chrome.contextMenus.create({
-			"title": service[i]["method"],
-			"id": service[i]["method"],
-			"parentId": "parent",
-			"contexts": contexts
-		});
-	}
+    var contexts = ["selection"];
+    chrome.contextMenus.removeAll();
+    // create parent
+    chrome.contextMenus.create({
+        "title": "Tieba_Service::call",
+        "id": "parent",
+        "contexts": contexts
+    });
+    // go to
+    chrome.contextMenus.create({
+        "title": "goto",
+        "parentId": "parent",
+        "id": "goto",
+        "contexts": contexts
+    });
+    // timestamp to time
+    chrome.contextMenus.create({
+        "title": "timestamp2time",
+        "parentId": "parent",
+        "id": "timestamp",
+        "contexts": contexts
+    });
+    // long to ip
+    chrome.contextMenus.create({
+        "title": "long2ip",
+        "parentId": "parent",
+        "id": "long2ip",
+        "contexts": contexts
+    });
+    // ip to long
+    chrome.contextMenus.create({
+        "title": "ip2long",
+        "parentId": "parent",
+        "id": "ip2long",
+        "contexts": contexts
+    });
+    chrome.contextMenus.create({
+        "type": "separator",
+        "parentId": "parent",
+        "id": "separator_sys",
+        "contexts": contexts
+    });
+    for(var i in service){
+        if ( typeof service[i]["type"] != "undefined" ){
+            if (service[i]["type"] == "separator")
+            {
+                chrome.contextMenus.create({
+                    "type": "separator",
+                    "id": "separator"+i,
+                    "parentId": "parent",
+                    "contexts": contexts
+                },
+                function(){
+                    alert(chrome.runtime.lastError.message);
+                });
+                continue;
+            }
+        }
+        chrome.contextMenus.create({
+            "title": service[i]["method"],
+            "id": service[i]["method"],
+            "parentId": "parent",
+            "contexts": contexts
+        });
+    }
 }
 
 function setService()
 {
-	var config = {"config":{"domain":domain,"service":service,"format":format}}
-	chrome.storage.local.set(config, function(){
-		getConfig();
-	});
+    var config = {"config":{"domain":domain,"service":service,"format":format}}
+    chrome.storage.local.set(config, function(){
+        getConfig();
+    });
 }
 
 function setDomain()
 {
-	new_domain = document.querySelector("#domain").value;
-	new_format = document.querySelector("#format").value;
-	
-	var config = {"config":{"domain":new_domain,"service":service,"format":new_format}};
-	chrome.storage.local.set(config, function(){
-		getConfig();
-	});
+    new_domain = document.querySelector("#domain").value;
+    new_format = document.querySelector("#format").value;
+    
+    var config = {"config":{"domain":new_domain,"service":service,"format":new_format}};
+    chrome.storage.local.set(config, function(){
+        getConfig();
+    });
 }
 
 function setDefault()
 {
-	chrome.storage.local.set(defaultConfig, function(){
-		getConfig();
-	});
+    chrome.storage.local.set(defaultConfig, function(){
+        getConfig();
+    });
 }
 
 function addSeparator()
 {
-	service.push({ "type": "separator" });
-	setService();
+    service.push({ "type": "separator" });
+    setService();
 }
 
 function addService()
 {
-	var service_name = document.querySelector("#service").value;
-	var method = document.querySelector("#method").value;
-	var param = document.querySelector("#param").value;
-	service.push({ "service": service_name, "method": method, "param": param });
-	setService();
+    var service_name = document.querySelector("#service").value;
+    var method = document.querySelector("#method").value;
+    var param = document.querySelector("#param").value;
+    service.push({ "service": service_name, "method": method, "param": param });
+    setService();
 }
 
 function deleteService(i)
 {
-	delete service[i];
-	setService();
+    delete service[i];
+    setService();
 }
 
 function moveUp(i)
 {
-	swap(i, i-1);
+    swap(i, i-1);
 }
 
 function moveDown(i)
 {
-	swap(i, i+1);
+    swap(i, i+1);
 }
 
 function swap(i, j)
 {
-	if(i<0||i>=service.length||j<0||j>=service.length){
-		return;
-	}
-	var temp = service[i];
-	service[i] = service[j];
-	service[j] = temp;
-	setService();
+    if(i<0||i>=service.length||j<0||j>=service.length){
+        return;
+    }
+    var temp = service[i];
+    service[i] = service[j];
+    service[j] = temp;
+    setService();
 }
 
 function opClickHandler(e)
 {
-	var targetId = e.target.id;
-	var idArr = targetId.split("_");
-	var op = idArr[0];
-	var id = parseInt(idArr[1]);
-	
-	if(op=="up"){
-		moveUp(id);
-	}
-	else if(op=="down"){
-		moveDown(id);
-	}
-	else if(op=="del"){
-		deleteService(id);
-	}
+    var targetId = e.target.id;
+    var idArr = targetId.split("_");
+    var op = idArr[0];
+    var id = parseInt(idArr[1]);
+    
+    if(op=="up"){
+        moveUp(id);
+    }
+    else if(op=="down"){
+        moveDown(id);
+    }
+    else if(op=="del"){
+        deleteService(id);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", getConfig);
--- src/service.js
+++ src/service.js
@@ -8,89 +8,89 @@
  **/
 
 defaultConfig = {
-	"config": {
-		"service" :[
-			{ "service": "forum", "method": "getBtxInfoByName", "param": "forum_name" },
-			{ "service": "forum", "method": "getBtxInfo", "param": "forum_id" },
-			{ "type": "separator" },
-			{ "service": "user", "method": "getUserData", "param": "user_id" },
-			{ "service": "user", "method": "getUserDataExByUname", "param": "user_name" },
-			{ "service": "user", "method": "getMyFavorForum", "param": "user_id" },
-			{ "type": "separator" },
-			{ "service": "perm", "method": "getForumMemberInfo", "param": "forum_id" },
-			{ "service": "perm", "method": "getBawuList", "param": "forum_id" },
-			{ "service": "perm", "method": "getUserBawuForum", "param": "user_id" }
-		],
-		"domain": "http://service.tieba.baidu.com",
-		"format": "json"
-	}
+    "config": {
+        "service" :[
+            { "service": "forum", "method": "getBtxInfoByName", "param": "forum_name" },
+            { "service": "forum", "method": "getBtxInfo", "param": "forum_id" },
+            { "type": "separator" },
+            { "service": "user", "method": "getUserData", "param": "user_id" },
+            { "service": "user", "method": "getUserDataExByUname", "param": "user_name" },
+            { "service": "user", "method": "getMyFavorForum", "param": "user_id" },
+            { "type": "separator" },
+            { "service": "perm", "method": "getForumMemberInfo", "param": "forum_id" },
+            { "service": "perm", "method": "getBawuList", "param": "forum_id" },
+            { "service": "perm", "method": "getUserBawuForum", "param": "user_id" }
+        ],
+        "domain": "http://service.tieba.baidu.com",
+        "format": "json"
+    }
 };
 
 function getConfig(info, callback)
 {
-	chrome.storage.local.get("config", function(data){
-		service = data.config["service"];
-		domain = data.config["domain"];
-		format = data.config["format"];
-		callback(info);
-	});
+    chrome.storage.local.get("config", function(data){
+        service = data.config["service"];
+        domain = data.config["domain"];
+        format = data.config["format"];
+        callback(info);
+    });
 }
 
 function parseQuery(info)
 {
-	var service_name = "";
-	var method = "";
-	var params = "";
-	var input = info.selectionText;
-	var menuId = info.menuItemId;
-	if(input.length == 0 || menuId == "parent"){
-		return;
-	}
-	if(menuId=="goto"){
-		chrome.tabs.create({
-			"url": "http://tieba.baidu.com/f?ie=utf-8&kw="+input
-		});
-		return;
-	}
-	else if(menuId=="timestamp"){
-		var realTime = new Date(input*1000);
-		var strTime = realTime.getFullYear()+"-"+(realTime.getMonth()+1)+"-"+realTime.getDate()+" "+realTime.getHours()+":"+realTime.getMinutes()+":"+realTime.getSeconds();
-		alert(strTime);
-		return;
-	}
-	else if(menuId=="long2ip"){
-		alert(long2ip(parseInt(input)));
-		return;
-	}
-	else if(menuId=="ip2long"){
-		alert(ip2long(input));
-		return;
-	}
-	else{
-		for(var i in service){
-			if(typeof service[i]["type"] == "undefined"){
-				if(service[i]["method"]==menuId){
-					service_name = service[i]["service"];
-					method = service[i]["method"];
-					param = service[i]["param"];
-					break;
-				}
-			}
-			else{
-				continue;
-			}
-		}
-		param += "="+input;
-		var url = domain+"/service/"+service_name+"?method="+method+"&"+param+"&format="+format+"&ie=utf-8";
-		chrome.tabs.create({
-			"url": url
-		});
-	}
+    var service_name = "";
+    var method = "";
+    var params = "";
+    var input = info.selectionText;
+    var menuId = info.menuItemId;
+    if(input.length == 0 || menuId == "parent"){
+        return;
+    }
+    if(menuId=="goto"){
+        chrome.tabs.create({
+            "url": "http://tieba.baidu.com/f?ie=utf-8&kw="+input
+        });
+        return;
+    }
+    else if(menuId=="timestamp"){
+        var realTime = new Date(input*1000);
+        var strTime = realTime.getFullYear()+"-"+(realTime.getMonth()+1)+"-"+realTime.getDate()+" "+realTime.getHours()+":"+realTime.getMinutes()+":"+realTime.getSeconds();
+        alert(strTime);
+        return;
+    }
+    else if(menuId=="long2ip"){
+        alert(long2ip(parseInt(input)));
+        return;
+    }
+    else if(menuId=="ip2long"){
+        alert(ip2long(input));
+        return;
+    }
+    else{
+        for(var i in service){
+            if(typeof service[i]["type"] == "undefined"){
+                if(service[i]["method"]==menuId){
+                    service_name = service[i]["service"];
+                    method = service[i]["method"];
+                    param = service[i]["param"];
+                    break;
+                }
+            }
+            else{
+                continue;
+            }
+        }
+        param += "="+input;
+        var url = domain+"/service/"+service_name+"?method="+method+"&"+param+"&format="+format+"&ie=utf-8";
+        chrome.tabs.create({
+            "url": url
+        });
+    }
 }
 
 function onClickHandler(info, tab)
 {
-	getConfig(info, parseQuery);
+    getConfig(info, parseQuery);
 }
 
 function callService()
@@ -98,98 +98,98 @@ function callService()
 }
 
 function long2ip(i){
-	return (Math.floor(i/(256*256*256))) + "." +
-	(Math.floor(i%(256*256*256)/(256*256))) + "." +
-	(Math.floor(i%(256*256)/256)) + "." +
-	(Math.floor(i%256));
+    return (Math.floor(i/(256*256*256))) + "." +
+    (Math.floor(i%(256*256*256)/(256*256))) + "." +
+    (Math.floor(i%(256*256)/256)) + "." +
+    (Math.floor(i%256));
 }
 
 function ip2long(ip)
 {
-	var numbers = ip.split(".");
-	return parseInt(numbers[0])*256*256*256 +
-	parseInt(numbers[1])*256*256 +
-	parseInt(numbers[2])*256 +
-	parseInt(numbers[3]);
+    var numbers = ip.split(".");
+    return parseInt(numbers[0])*256*256*256 +
+    parseInt(numbers[1])*256*256 +
+    parseInt(numbers[2])*256 +
+    parseInt(numbers[3]);
 }    
 
 function generateContextMenu()
 {
-	var contexts = ["selection"];
-	chrome.contextMenus.removeAll();
-	// create parent
-	chrome.contextMenus.create({
-		"title": "Tieba_Service::call",
-		"id": "parent",
-		"contexts":contexts
-	});
-	// go to
-	chrome.contextMenus.create({
-		"title": "goto",
-		"parentId": "parent",
-		"id": "goto",
-		"contexts": contexts
-	});
-	// timestamp to time
-	chrome.contextMenus.create({
-		"title": "timestamp2time",
-		"parentId": "parent",
-		"id": "timestamp",
-		"contexts": contexts
-	});
-	// long to ip
-	chrome.contextMenus.create({
-		"title": "long2ip",
-		"parentId": "parent",
-		"id": "long2ip",
-		"contexts": contexts
-	});
-	// ip to long
-	chrome.contextMenus.create({
-		"title": "ip2long",
-		"parentId": "parent",
-		"id": "ip2long",
-		"contexts": contexts
-	});
-	chrome.contextMenus.create({
-		"type": "separator",
-		"parentId": "parent",
-		"id": "separator_sys",
-		"contexts": contexts
-	});
-	for(var i in service){
-		if ( typeof service[i]["type"] != "undefined" ){
-			if (service[i]["type"] == "separator")
-			{
-				chrome.contextMenus.create({
-					"type": "separator",
-					"parentId": "parent",
-					"id": "separator"+i,
-					"contexts": contexts
-				});
-				continue;
-			}
-		}
-		chrome.contextMenus.create({
-			"title": service[i]["method"],
-			"id": service[i]["method"],
-			"parentId": "parent",
-			"contexts": contexts
-		});
-	}
+    var contexts = ["selection"];
+    chrome.contextMenus.removeAll();
+    // create parent
+    chrome.contextMenus.create({
+        "title": "Tieba_Service::call",
+        "id": "parent",
+        "contexts":contexts
+    });
+    // go to
+    chrome.contextMenus.create({
+        "title": "goto",
+        "parentId": "parent",
+        "id": "goto",
+        "contexts": contexts
+    });
+    // timestamp to time
+    chrome.contextMenus.create({
+        "title": "timestamp2time",
+        "parentId": "parent",
+        "id": "timestamp",
+        "contexts": contexts
+    });
+    // long to ip
+    chrome.contextMenus.create({
+        "title": "long2ip",
+        "parentId": "parent",
+        "id": "long2ip",
+        "contexts": contexts
+    });
+    // ip to long
+    chrome.contextMenus.create({
+        "title": "ip2long",
+        "parentId": "parent",
+        "id": "ip2long",
+        "contexts": contexts
+    });
+    chrome.contextMenus.create({
+        "type": "separator",
+        "parentId": "parent",
+        "id": "separator_sys",
+        "contexts": contexts
+    });
+    for(var i in service){
+        if ( typeof service[i]["type"] != "undefined" ){
+            if (service[i]["type"] == "separator")
+            {
+                chrome.contextMenus.create({
+                    "type": "separator",
+                    "parentId": "parent",
+                    "id": "separator"+i,
+                    "contexts": contexts
+                });
+                continue;
+            }
+        }
+        chrome.contextMenus.create({
+            "title": service[i]["method"],
+            "id": service[i]["method"],
+            "parentId": "parent",
+            "contexts": contexts
+        });
+    }
 }
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 chrome.runtime.onInstalled.addListener(function(){
-	service = defaultConfig["config"]["service"];
-	generateContextMenu();
-	chrome.storage.local.set(defaultConfig);
+    service = defaultConfig["config"]["service"];
+    generateContextMenu();
+    chrome.storage.local.set(defaultConfig);
 });
 
 chrome.browserAction.onClicked.addListener(function(){
-	chrome.tabs.create({
-		url: "options.html"
-	});
+    chrome.tabs.create({
+        url: "options.html"
+    });
 });
 
